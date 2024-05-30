f86f3e2d478cd9ac389c98593e315a832e180b94
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-05-30 13:19:33 +0200

    "185"

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPagesComponent } from '../auth/pages/layout-pages/layout-pages.component';
 
-const routes: Routes = [];
+// localhost: 4200/heroes
+const routes: Routes= [
+  {path: '', component: LayoutPagesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
