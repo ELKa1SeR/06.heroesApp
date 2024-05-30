007a42505268da24e5595853770b7a8ec1fd5116
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-05-30 13:41:50 +0200

    "186"

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPagesComponent } from '../auth/pages/layout-pages/layout-pages.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 // localhost: 4200/heroes
 const routes: Routes= [
-  {path: '', component: LayoutPagesComponent}
+  {path: '', component: LayoutPagesComponent,
+  children:[
+
+    {path: 'new-hero', component: NewPageComponent},
+    {path: 'search', component: SearchPageComponent},
+    {path: 'list', component: ListPageComponent},
+    {path: 'edit/:id', component: NewPageComponent},
+    {path: ':id', component: HeroPageComponent},
+    {path: '**', redirectTo: 'list'}
+  ]}
 ];
 
 @NgModule({
